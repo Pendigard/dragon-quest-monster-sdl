486dfacd9e6be2a79c1a0e59f9a728c3ee9e0d02
ItsMeLeandr <leandro.thollot.panepint@gmail.com>
2023-03-14 15:58:35 +0100

    Ajout des .h et .cpp de Synthesis et le d'un .json

---
+++ src/Synthesis.cpp
@@ -0,0 +1,5 @@
+#include "Synthesis.h"
+
+Monster Synthesis::makeSynthesis(Monster parent1, Monster parent2) {
+    
+}
---
+++ src/Synthesis.h
@@ -0,0 +1,8 @@
+#ifndef _SYNTHESIS
+#define _SYNTHESIS
+
+#include "Monster.h"
+
+Monster makeSynthesis(Monster parent1, Monster parent2);
+
+#endif
